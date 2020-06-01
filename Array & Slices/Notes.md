f55f27a3f21c925f75109790654ddfc893327e5e
duke-alibubu <ttdungalibubu@gmail.com>
2020-06-02 05:45:40 +0800

    slices brief

---
+++ Array & Slices/Notes.md
@@ -0,0 +1,6 @@
+# Array
+- Is of FIXED size. Passed as value, not reference!!!!
+# Slices
+- Like, a reference to an array. Passed as reference.
+- The length of the slice is the number of elements in the slice.
+- The capacity of the slice is the number of elements in the underlying array starting from the index from which the slice is created.
---
+++ Array & Slices/slices.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fruitarray := [...]string{"apple", "orange", "grape", "mango", "water melon", "pine apple", "chikoo"}
+	fruitslice := fruitarray[1:3]
+	fmt.Printf("length of slice %d capacity %d\n", len(fruitslice), cap(fruitslice)) //length of is 2 and capacity is 6
+	fruitslice = fruitslice[1:cap(fruitslice)]                                       //re-slicing furitslice till its capacity
+	fmt.Println("After re-slicing length is", len(fruitslice), "and capacity is", cap(fruitslice))
+	fmt.Println(fruitslice)
+}
